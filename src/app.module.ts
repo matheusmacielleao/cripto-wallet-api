89a6402ec12fe630d54da2a0276ce0a18d662f7b
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-20 17:31:44 -0300

    init :initial comit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WalletsModule } from './app/modules/wallet.module';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './app/modules/database.module';
+
+@Module({
+  imports: [
+    WalletsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
