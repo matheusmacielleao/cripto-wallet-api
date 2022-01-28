e2ec4393d2a3384702ae6ed1033149ba1926251c
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-28 11:03:31 -0300

    feat: routes for delete and find by id

--- src/app/controllers/wallet.controller.ts
+++ src/app/controllers/wallet.controller.ts
@@ -14,6 +14,7 @@ import CreateWalletDto from '../dto/wallet/createWalletDto';
 import OperationDto from '../dto/wallet/OperationDto';
 import { paginateSerialize } from '../serializer/walletSerializer';
 import GetWalletDto from '../dto/wallet/getWalletDto';
+import { string } from '@hapi/joi';
 @Controller('wallets')
 export default class WalletsController {
   constructor(private readonly walletService: WalletsService) {}
@@ -23,24 +24,41 @@ export default class WalletsController {
   async createWallet(@Body() post: CreateWalletDto) {
     return await this.walletService.create(post);
   }
+  @Post(':address')
+  async transference(
+    @Param('address') adress: string,
+    @Body() operation: OperationDto,
+  ) {
+    return await this.walletService.transference(adress, operation);
+  }
 
   @Get()
   async getAll(@Query() payload: any) {
     const wallets = await this.walletService.getAll(payload);
     return wallets;
   }
-  @Put(':adress')
+
+  @Get(':address')
+  async getById(@Param('address') adress: string) {
+    return await this.walletService.getOne(adress);
+  }
+
+  @Get(':address/transaction')
+  async getTransactions(@Param('address') adress: string) {
+    return await this.walletService.getTransactions(adress);
+  }
+
+  @Put(':address')
   async withdrawOrDeposit(
-    @Param('adress') adress: string,
+    @Param('address') adress: string,
     @Body() operation: OperationDto[],
   ) {
     return await this.walletService.withdrawOrDeposit(adress, operation);
   }
-  @Post(':adress')
-  async transference(
-    @Param('adress') adress: string,
-    @Body() operation: OperationDto,
-  ) {
-    return await this.walletService.transference(adress, operation);
+
+  @Delete(':address')
+  @HttpCode(204)
+  async delete(@Param('address') address: string) {
+    await this.walletService.delete(address);
   }
 }
